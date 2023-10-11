24c37b691e8ea8274988aad421ef80d638273c30
Voinash Denys <desdenisandqwerty@gmail.com>
2023-10-11 21:15:29 +0300

    task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const elements = {
+  galleryList: document.querySelector('.gallery'),
+};
+elements.galleryList.innerHTML = createGallery(galleryItems);
+
+function createGallery(arr) {
+  return arr
+    .map(
+      ({ original, preview, description }, idx) => `
+      <li class="gallery__item" data-id="${idx}>
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}">
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
