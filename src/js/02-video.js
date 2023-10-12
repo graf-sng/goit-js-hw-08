bc9723e78d9b942de2f230d518022f5cf781c222
Voinash Denys <desdenisandqwerty@gmail.com>
2023-10-12 20:48:18 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LS_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(LS_KEY, data.seconds ?? 0);
+  }, 1000)
+);
+
+if (localStorage.getItem(LS_KEY)) {
+  const time = localStorage.getItem(LS_KEY);
+  player.setCurrentTime(time);
+}
